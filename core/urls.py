60d978346dd18cf9bd44acfa36122cea70030a64
Sinon21-byte <nicolas.maruri.a@gmail.com>
2025-06-20 21:40:26 -0400

    Revert "Add activities report"

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.login_view, name='login'),
     path('formulario/', views.formulario_view, name='formulario'),
-    path('actividades/', views.actividades_view, name='actividades'),
 ]
