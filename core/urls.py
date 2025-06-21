c9a5039ab8d4e48dfe2f4a11df0e8e6f405b0681
Sinon21-byte <nicolas.maruri.a@gmail.com>
2025-06-20 21:12:19 -0400

    Add route for reporte_actividades

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.login_view, name='login'),
     path('formulario/', views.formulario_view, name='formulario'),
+    path('actividades/', views.reporte_actividades_view, name='reporte_actividades'),
 ]
